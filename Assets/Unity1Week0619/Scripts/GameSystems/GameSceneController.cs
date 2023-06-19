0408fc0349f039eb69d4c471fc249300c06cb8e2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-19 23:08:10 +0900

    とりあえずランダムで生成してみる

---
+++ Assets/Unity1Week0619/Scripts/GameSystems/GameSceneController.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Unity1Week0619.GameSystems
+{
+    /// <summary>
+    /// ゲームシーンを制御するクラス
+    /// </summary>
+    public class GameSceneController : MonoBehaviour
+    {
+        [SerializeField]
+        private SacabambaspisSpawner sacabambaspisSpawner;
+
+        void Start()
+        {
+            var ct = this.GetCancellationTokenOnDestroy();
+            this.sacabambaspisSpawner.BeginSpawn(ct);
+        }
+    }
+}
---
+++ Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Unity1Week0619.GameSystems
+{
+    public class SacabambaspisSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject sacabambaspisPrefab;
+
+        [SerializeField]
+        private float spawnInterval;
+
+        [SerializeField]
+        private Rect spawnArea;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            var center = this.transform.localPosition + new Vector3(spawnArea.center.x, spawnArea.center.y, 0f);
+            Gizmos.DrawWireCube( center, spawnArea.size);
+        }
+
+        public void BeginSpawn(CancellationToken cancellationToken)
+        {
+            UniTask.Void(async _ =>
+                {
+                    while (true)
+                    {
+                        await UniTask.Delay(TimeSpan.FromSeconds(spawnInterval), cancellationToken: cancellationToken);
+                        var position = this.transform.localPosition;
+                        position.x += UnityEngine.Random.Range(spawnArea.xMin, spawnArea.xMax);
+                        position.y += UnityEngine.Random.Range(spawnArea.yMin, spawnArea.yMax);
+                        Instantiate(sacabambaspisPrefab, position, Quaternion.identity);
+                    }
+                },
+                cancellationToken);
+        }
+    }
+}
