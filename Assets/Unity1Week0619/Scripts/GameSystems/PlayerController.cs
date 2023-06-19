d95c5a22c4398339bb7a6c868762e0af145a21db
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-19 23:21:32 +0900

    とりあえずプレイヤー追加

---
+++ Assets/Unity1Week0619/Scripts/GameSystems/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Unity1Week0619.GameSystems
+{
+    /// <summary>
+    /// プレイヤーを制御するクラス
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D targetRigidbody2D;
+
+        [SerializeField]
+        private float smoothDamp;
+
+        private Vector2 velocity;
+
+        // マウスの座標と同期を取る
+        private void Update()
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            var pos2D = new Vector2(pos.x, pos.y);
+
+            // Rigidbody2dに対して、SmoothDampを使って移動させる
+            targetRigidbody2D.velocity = (pos2D - targetRigidbody2D.position) * this.smoothDamp;
+        }
+    }
+}
