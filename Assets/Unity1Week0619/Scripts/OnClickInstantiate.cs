773095c0e1e95d71e1e38817331cbf51da748d63
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-19 20:42:58 +0900

    とりあえずScriptsフォルダ追加

---
+++ Assets/Unity1Week0619/Scripts/OnClickInstantiate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Unity1Week0619
+{
+    public class OnClickInstantiate : MonoBehaviour
+    {
+        // クリックした座標にプレハブを生成する
+        [SerializeField] GameObject prefab;
+
+        [SerializeField]
+        private Camera targetCamera;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var pos = Input.mousePosition;
+                pos.z = 10f;
+                var obj = Instantiate(prefab);
+                obj.transform.position = this.targetCamera.ScreenToWorldPoint(pos);
+            }
+        }
+    }
+}
---
+++ Assets/Unity1Week0619/Scripts/SacabambaspisController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Unity1Week0619
+{
+    /// <summary>
+    /// サカバンバスピスを制御するクラス
+    /// </summary>
+    public class SacabambaspisController : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log(collision.gameObject.name);
+        }
+    }
+}
