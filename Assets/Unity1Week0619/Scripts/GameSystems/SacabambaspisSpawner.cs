06aa9154b52a32a0a057a43a3eb819d07ed2e9bb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-20 12:33:21 +0900

    最初に生成する座標は固定する

--- Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisSpawner.cs
+++ Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisSpawner.cs
@@ -13,6 +13,12 @@ namespace Unity1Week0619.GameSystems
         [SerializeField]
         private GameObject sacabambaspisPrefab;
 
+        /// <summary>
+        /// 最初に生成する座標を持つオブジェクト
+        /// </summary>
+        [SerializeField]
+        private Transform firstSpawnPoint;
+
         [SerializeField]
         private float spawnInterval;
 
@@ -30,16 +36,35 @@ namespace Unity1Week0619.GameSystems
         {
             UniTask.Void(async _ =>
                 {
+                    var spawnCount = 0;
                     while (true)
                     {
                         await UniTask.Delay(TimeSpan.FromSeconds(spawnInterval), cancellationToken: cancellationToken);
-                        var position = this.transform.localPosition;
-                        position.x += UnityEngine.Random.Range(spawnArea.xMin, spawnArea.xMax);
-                        position.y += UnityEngine.Random.Range(spawnArea.yMin, spawnArea.yMax);
+                        
+                        var position = this.GetSpawnPosition(spawnCount);
                         Instantiate(sacabambaspisPrefab, position, Quaternion.identity, this.parent);
+                        spawnCount++;
                     }
                 },
                 cancellationToken);
+        }
+        
+        /// <summary>
+        /// 生成する座標を返す
+        /// </summary>
+        private Vector3 GetSpawnPosition(int spawnCount)
+        {
+            // 初回は分かりやすくするために指定した座標を返す
+            if (spawnCount == 0)
+            {
+                return this.firstSpawnPoint.position;
+            }
+            
+            // それ以外はランダムな座標を返す
+            var position = this.transform.localPosition;
+            position.x += UnityEngine.Random.Range(spawnArea.xMin, spawnArea.xMax);
+            position.y += UnityEngine.Random.Range(spawnArea.yMin, spawnArea.yMax);
+            return position;
         }
     }
 }
