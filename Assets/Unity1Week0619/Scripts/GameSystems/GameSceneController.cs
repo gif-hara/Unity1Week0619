52ac795b98d30572e80e8173d245e52e745c3fdb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-20 19:23:33 +0900

    try catchする

--- Assets/Unity1Week0619/Scripts/GameSystems/GameSceneController.cs
+++ Assets/Unity1Week0619/Scripts/GameSystems/GameSceneController.cs
@@ -17,7 +17,7 @@ namespace Unity1Week0619.GameSystems
     {
         [SerializeField]
         private GameDesignData gameDesignData;
-        
+
         [SerializeField]
         private SacabambaspisSpawner sacabambaspisSpawner;
 
@@ -26,103 +26,114 @@ namespace Unity1Week0619.GameSystems
 
         async void Start()
         {
-            // ブートシステム初期化待ち
-            await BootSystem.IsReady;
+            try
+            {
+                // ブートシステム初期化待ち
+                await BootSystem.IsReady;
 
-            // ゲームシステム初期化
-            var gameSceneToken = this.GetCancellationTokenOnDestroy();
-            var score = new AsyncReactiveProperty<int>(0);
-            var baspisGauge = new AsyncReactiveProperty<float>(this.gameDesignData.BaspisGaugeData.InitialAmount);
-            var fullBaspisModeGauge = new AsyncReactiveProperty<float>(0.0f);
-            IDisposable fullBaspisModeStream = null;
-            GameUIPresenter.Setup(
-                this.gameUIView,
-                score,
-                baspisGauge,
-                fullBaspisModeGauge,
-                gameSceneToken
-                );
-            
-            // サカバンバスピスがプレイヤーに入った際の処理
-            MessageBroker.GetSubscriber<GameEvents.OnEnterSacabambaspis>()
-                .Subscribe(x =>
-                {
-                    score.Value += this.gameDesignData.GetSacabambaspisData(x.SacabambaspisType).Score;
-                    if(fullBaspisModeStream != null)
-                    {
-                        fullBaspisModeGauge.Value = Mathf.Clamp01(fullBaspisModeGauge.Value + this.gameDesignData.FullBaspisModeData.OnEnterAmount);
-                    }
-                    else
-                    {
-                        baspisGauge.Value = Mathf.Clamp01(baspisGauge.Value + this.gameDesignData.BaspisGaugeData.OnEnterAmount);
-                    }
+                // ゲームシステム初期化
+                var gameSceneToken = this.GetCancellationTokenOnDestroy();
+                var score = new AsyncReactiveProperty<int>(0);
+                var baspisGauge = new AsyncReactiveProperty<float>(this.gameDesignData.BaspisGaugeData.InitialAmount);
+                var fullBaspisModeGauge = new AsyncReactiveProperty<float>(0.0f);
+                IDisposable fullBaspisModeStream = null;
+                GameUIPresenter.Setup(
+                    this.gameUIView,
+                    score,
+                    baspisGauge,
+                    fullBaspisModeGauge,
+                    gameSceneToken
+                    );
 
-                    if (baspisGauge >= 1.0f && fullBaspisModeStream == null)
+                // サカバンバスピスがプレイヤーに入った際の処理
+                MessageBroker.GetSubscriber<GameEvents.OnEnterSacabambaspis>()
+                    .Subscribe(x =>
                     {
-                        // フルバスピスモード開始
-                        MessageBroker.GetPublisher<GameEvents.BeginFullBaspisMode>()
-                            .Publish(GameEvents.BeginFullBaspisMode.Get());
-                        fullBaspisModeGauge.Value = 1.0f;
-                        fullBaspisModeStream = this.GetAsyncUpdateTrigger()
-                            .Subscribe(_ =>
-                            {
-                                fullBaspisModeGauge.Value -= this.gameDesignData.FullBaspisModeData.DecreaseAmountPerSeconds * UnityEngine.Time.deltaTime;
-                                if (fullBaspisModeGauge <= 0.0f)
+                        score.Value += this.gameDesignData.GetSacabambaspisData(x.SacabambaspisType).Score;
+                        if (fullBaspisModeStream != null)
+                        {
+                            fullBaspisModeGauge.Value = Mathf.Clamp01(fullBaspisModeGauge.Value + this.gameDesignData.FullBaspisModeData.OnEnterAmount);
+                        }
+                        else
+                        {
+                            baspisGauge.Value = Mathf.Clamp01(baspisGauge.Value + this.gameDesignData.BaspisGaugeData.OnEnterAmount);
+                        }
+
+                        if (baspisGauge >= 1.0f && fullBaspisModeStream == null)
+                        {
+                            // フルバスピスモード開始
+                            MessageBroker.GetPublisher<GameEvents.BeginFullBaspisMode>()
+                                .Publish(GameEvents.BeginFullBaspisMode.Get());
+                            fullBaspisModeGauge.Value = 1.0f;
+                            fullBaspisModeStream = this.GetAsyncUpdateTrigger()
+                                .Subscribe(_ =>
                                 {
-                                    // フルバスピスモード終了
-                                    fullBaspisModeStream?.Dispose();
-                                    fullBaspisModeStream = null;
-                                    baspisGauge.Value = this.gameDesignData.BaspisGaugeData.InitialAmount;
-                                    MessageBroker.GetPublisher<GameEvents.EndFullBaspisMode>()
-                                        .Publish(GameEvents.EndFullBaspisMode.Get());
-                                }
-                            });
-                    }
-                })
-                .AddTo(gameSceneToken);
-            
-            // サカバンバスピスが離れた際の処理
-            MessageBroker.GetSubscriber<GameEvents.OnExitSacabambaspis>()
-                .Subscribe(x =>
-                {
-                    score.Value -= this.gameDesignData.GetSacabambaspisData(x.SacabambaspisType).Score;
-                    if(fullBaspisModeStream != null)
-                    {
-                        fullBaspisModeGauge.Value = Mathf.Clamp01(fullBaspisModeGauge - this.gameDesignData.FullBaspisModeData.OnExitAmount);
-                    }
-                    else
-                    {
-                        baspisGauge.Value = Mathf.Clamp01(baspisGauge.Value - this.gameDesignData.BaspisGaugeData.OnExitAmount);
-                    }
-                })
-                .AddTo(gameSceneToken);
-            
-            // ゲーム終了時の処理
-            MessageBroker.GetAsyncSubscriber<GameEvents.TakeUntilEndGame>()
-                .Subscribe(async (_, ct) =>
-                {
-                    await baspisGauge
-                        .Where(x => x <= 0.0f)
-                        .FirstAsync(ct);
-                })
-                .AddTo(gameSceneToken);
+                                    fullBaspisModeGauge.Value -= this.gameDesignData.FullBaspisModeData.DecreaseAmountPerSeconds * UnityEngine.Time.deltaTime;
+                                    if (fullBaspisModeGauge <= 0.0f)
+                                    {
+                                        // フルバスピスモード終了
+                                        fullBaspisModeStream?.Dispose();
+                                        fullBaspisModeStream = null;
+                                        baspisGauge.Value = this.gameDesignData.BaspisGaugeData.InitialAmount;
+                                        MessageBroker.GetPublisher<GameEvents.EndFullBaspisMode>()
+                                            .Publish(GameEvents.EndFullBaspisMode.Get());
+                                    }
+                                });
+                        }
+                    })
+                    .AddTo(gameSceneToken);
 
-            // ゲームを開始する
-            await MessageBroker.GetAsyncPublisher<GameEvents.BeginGame>()
-                .PublishAsync(GameEvents.BeginGame.Get(), gameSceneToken);
+                // サカバンバスピスが離れた際の処理
+                MessageBroker.GetSubscriber<GameEvents.OnExitSacabambaspis>()
+                    .Subscribe(x =>
+                    {
+                        score.Value -= this.gameDesignData.GetSacabambaspisData(x.SacabambaspisType).Score;
+                        if (fullBaspisModeStream != null)
+                        {
+                            fullBaspisModeGauge.Value = Mathf.Clamp01(fullBaspisModeGauge - this.gameDesignData.FullBaspisModeData.OnExitAmount);
+                        }
+                        else
+                        {
+                            baspisGauge.Value = Mathf.Clamp01(baspisGauge.Value - this.gameDesignData.BaspisGaugeData.OnExitAmount);
+                        }
+                    })
+                    .AddTo(gameSceneToken);
 
-            var inGameTokenSource = new CancellationTokenSource();
-            this.sacabambaspisSpawner.BeginSpawn(this.gameDesignData, inGameTokenSource.Token);
-            
-            // ゲームが終了するまで待機
-            await MessageBroker.GetAsyncPublisher<GameEvents.TakeUntilEndGame>()
-                .PublishAsync(GameEvents.TakeUntilEndGame.Get(), gameSceneToken);
-            
-            inGameTokenSource.Dispose();
-            
-            // ゲーム終了
-            await MessageBroker.GetAsyncPublisher<GameEvents.EndGame>()
-                .PublishAsync(GameEvents.EndGame.Get(), gameSceneToken);
+                // ゲーム終了時の処理
+                MessageBroker.GetAsyncSubscriber<GameEvents.TakeUntilEndGame>()
+                    .Subscribe(async (_, ct) =>
+                    {
+                        await baspisGauge
+                            .Where(x => x <= 0.0f)
+                            .FirstOrDefaultAsync(ct);
+                    })
+                    .AddTo(gameSceneToken);
+
+                // ゲームを開始する
+                await MessageBroker.GetAsyncPublisher<GameEvents.BeginGame>()
+                    .PublishAsync(GameEvents.BeginGame.Get(), gameSceneToken);
+
+                var inGameTokenSource = new CancellationTokenSource();
+                this.sacabambaspisSpawner.BeginSpawn(this.gameDesignData, inGameTokenSource.Token);
+
+                // ゲームが終了するまで待機
+                await MessageBroker.GetAsyncPublisher<GameEvents.TakeUntilEndGame>()
+                    .PublishAsync(GameEvents.TakeUntilEndGame.Get(), gameSceneToken);
+
+                inGameTokenSource.Dispose();
+
+                // ゲーム終了
+                await MessageBroker.GetAsyncPublisher<GameEvents.EndGame>()
+                    .PublishAsync(GameEvents.EndGame.Get(), gameSceneToken);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
         }
     }
 }
