337372cbcc406319a432e71571307e3a65f0da9f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-20 16:23:16 +0900

    プレイヤーの中に入った際にエフェクトを生成するように

--- Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisController.cs
+++ Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using Cysharp.Threading.Tasks.Triggers;
 using Unity1Week0619.GameSystems;
 using Unity1Week0619.Scripts.GameSystems;
 using UnityEngine;
@@ -21,7 +23,10 @@ namespace Unity1Week0619
         private float spawnDelaySeconds;
         
         [SerializeField]
-        private GameObject effectPrefab;
+        private GameObject spawnEffectPrefab;
+
+        [SerializeField]
+        private GameObject onEnterEffectPrefab;
         
         /// <summary>
         /// プレイヤーの中に入ったか
@@ -31,7 +36,7 @@ namespace Unity1Week0619
         private async void Start()
         {
             // 最初にエフェクトを生成する
-            Instantiate(this.effectPrefab, this.transform.position, Quaternion.identity);
+            Instantiate(this.spawnEffectPrefab, this.transform.position, Quaternion.identity);
             this.gameObject.SetActive(false);
             this.targetRigidbody2D.simulated = false;
             // TODO: SE再生
@@ -41,45 +46,54 @@ namespace Unity1Week0619
             this.gameObject.SetActive(true);
             this.targetRigidbody2D.simulated = true;
             // TODO: SE再生する？
+
+            this.GetAsyncTriggerEnter2DTrigger()
+                .Subscribe(other =>
+                {
+                    if (other.attachedRigidbody != null)
+                    {
+                        // プレイヤーと接触した場合はプレイヤーの中に入ったとみなす
+                        if (!this.isEnterPlayer && other.attachedRigidbody.GetComponent<PlayerController>() != null)
+                        {
+                            this.OnEnterPlayer();
+                        }
+                        // 他のサカバンバスピスに接触した場合はプレイヤーの中に入っているかを確認する
+                        else if (!this.isEnterPlayer &&
+                                 other.attachedRigidbody.GetComponent<SacabambaspisController>() != null)
+                        {
+                            var otherSacabambaspis = other.attachedRigidbody.GetComponent<SacabambaspisController>();
+                            // 他のサカバンバスピスがプレイヤーの中に入っている場合は自分もプレイヤーの中に入る
+                            if (otherSacabambaspis.isEnterPlayer)
+                            {
+                                this.OnEnterPlayer();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (other.CompareTag("Deadline"))
+                        {
+                            Destroy(this.gameObject);
+                            if (this.isEnterPlayer)
+                            {
+                                MessageBroker.GetPublisher<GameEvents.OnExitSacabambaspis>()
+                                    .Publish(GameEvents.OnExitSacabambaspis.Get(this.sacabambaspisType));
+                            }
+                        }
+                    }
+                })
+                .AddTo(this.GetCancellationTokenOnDestroy());
         }
 
-        private void OnTriggerEnter2D(Collider2D collider)
+        /// <summary>
+        /// プレイヤーの中に入った際の処理
+        /// </summary>
+        private void OnEnterPlayer()
         {
-            if (collider.attachedRigidbody != null)
-            {
-                // プレイヤーと接触した場合はプレイヤーの中に入ったとみなす
-                if(!this.isEnterPlayer && collider.attachedRigidbody.GetComponent<PlayerController>() != null)
-                {
-                    this.isEnterPlayer = true;
-                    MessageBroker.GetPublisher<GameEvents.OnEnterSacabambaspis>()
-                        .Publish(GameEvents.OnEnterSacabambaspis.Get(this.sacabambaspisType));
-
-                }
-                // 他のサカバンバスピスに接触した場合はプレイヤーの中に入っているかを確認する
-                else if (!this.isEnterPlayer && collider.attachedRigidbody.GetComponent<SacabambaspisController>() != null)
-                {
-                    var other = collider.attachedRigidbody.GetComponent<SacabambaspisController>();
-                    // 他のサカバンバスピスがプレイヤーの中に入っている場合は自分もプレイヤーの中に入る
-                    if (other.isEnterPlayer)
-                    {
-                        this.isEnterPlayer = true;
-                        MessageBroker.GetPublisher<GameEvents.OnEnterSacabambaspis>()
-                            .Publish(GameEvents.OnEnterSacabambaspis.Get(this.sacabambaspisType));
-                    }
-                }
-            }
-            else
-            {
-                if (collider.CompareTag("Deadline"))
-                {
-                    Destroy(this.gameObject);
-                    if (this.isEnterPlayer)
-                    {
-                        MessageBroker.GetPublisher<GameEvents.OnExitSacabambaspis>()
-                            .Publish(GameEvents.OnExitSacabambaspis.Get(this.sacabambaspisType));
-                    }
-                }
-            }
+            this.isEnterPlayer = true;
+            Instantiate(this.onEnterEffectPrefab, this.transform.position, Quaternion.identity);
+            MessageBroker.GetPublisher<GameEvents.OnEnterSacabambaspis>()
+                .Publish(GameEvents.OnEnterSacabambaspis.Get(this.sacabambaspisType));
         }
     }
 }
