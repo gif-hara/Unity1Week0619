f40b087932dbdd1a2090f2ca1921f7a7695a090e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-20 12:02:42 +0900

    Define追加

---
+++ Assets/Unity1Week0619/Scripts/Define.cs
@@ -0,0 +1,21 @@
+namespace Unity1Week0619
+{
+    public static class Define
+    {
+        /// <summary>
+        /// サカバンバスピスのタイプ
+        /// </summary>
+        public enum SacabambaspisType
+        {
+            /// <summary>
+            /// 通常
+            /// </summary>
+            Normal,
+            
+            /// <summary>
+            /// 色付き
+            /// </summary>
+            Colorful,
+        }
+    }
+}
