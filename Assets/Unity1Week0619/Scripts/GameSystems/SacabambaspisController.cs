2e6d4440c894cb1e59554ef3faeb9ea375af3d4f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-21 17:08:08 +0900

    ゲーム終了していたら死亡するように

--- Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisController.cs
+++ Assets/Unity1Week0619/Scripts/GameSystems/SacabambaspisController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
 using Cysharp.Threading.Tasks.Triggers;
+using MessagePipe;
 using UnityEngine;
 
 namespace Unity1Week0619.GameSystems
@@ -34,71 +35,98 @@ namespace Unity1Week0619.GameSystems
 
         public async UniTask SetupAsync(GameDesignData gameDesignData)
         {
-            // 最初にエフェクトを生成する
-            Instantiate(this.spawnEffectPrefab, this.transform.position, Quaternion.identity);
-            this.gameObject.SetActive(false);
-            this.targetRigidbody2D.simulated = false;
-            var sacabambaspisData = gameDesignData.GetSacabambaspisData(this.sacabambaspisType);
-            PlaySE(sacabambaspisData.SpawnAudioDataList);
-
-            await UniTask.Delay(TimeSpan.FromSeconds(sacabambaspisData.SpawnDelaySeconds));
-
-            // ここで登場する
-            this.gameObject.SetActive(true);
-            this.targetRigidbody2D.simulated = true;
-            this.targetRigidbody2D.velocity = new Vector2(
-                UnityEngine.Random.Range(sacabambaspisData.SpawnVelocityMin.x, sacabambaspisData.SpawnVelocityMax.x),
-                UnityEngine.Random.Range(sacabambaspisData.SpawnVelocityMin.y, sacabambaspisData.SpawnVelocityMax.y)
-                );
-            this.targetRigidbody2D.AddTorque(
-                UnityEngine.Random.Range(sacabambaspisData.SpawnAddTorqueMin, sacabambaspisData.SpawnAddTorqueMax)
-                );
-            var appearanceAudioData = sacabambaspisData.AppearanceAudioDataList[UnityEngine.Random.Range(0, sacabambaspisData.AppearanceAudioDataList.Count)];
-            AudioManager.PlaySE(appearanceAudioData.AudioData);
-            this.speechBalloonController.PlayAnimation(appearanceAudioData.Serif);
-
-            this.GetAsyncTriggerEnter2DTrigger()
-                .Subscribe(other =>
-                {
-                    if (other.attachedRigidbody != null)
+            try
+            {
+                var token = this.GetCancellationTokenOnDestroy();
+                
+                // ゲームが終了していたら死亡する
+                MessageBroker.GetSubscriber<GameEvents.NotifyEndGame>()
+                    .Subscribe(_ =>
                     {
-                        if (!this.isEnterPlayer)
+                        Destroy(this.gameObject);
+                    })
+                    .AddTo(token);
+                
+                // 最初にエフェクトを生成する
+                Instantiate(this.spawnEffectPrefab, this.transform.position, Quaternion.identity);
+                this.gameObject.SetActive(false);
+                this.targetRigidbody2D.simulated = false;
+                var sacabambaspisData = gameDesignData.GetSacabambaspisData(this.sacabambaspisType);
+                PlaySE(sacabambaspisData.SpawnAudioDataList);
+
+                await UniTask.Delay(TimeSpan.FromSeconds(sacabambaspisData.SpawnDelaySeconds), cancellationToken: token);
+
+                // ここで登場する
+                this.gameObject.SetActive(true);
+                this.targetRigidbody2D.simulated = true;
+                this.targetRigidbody2D.velocity = new Vector2(
+                    UnityEngine.Random.Range(sacabambaspisData.SpawnVelocityMin.x,
+                        sacabambaspisData.SpawnVelocityMax.x),
+                    UnityEngine.Random.Range(sacabambaspisData.SpawnVelocityMin.y, sacabambaspisData.SpawnVelocityMax.y)
+                );
+                this.targetRigidbody2D.AddTorque(
+                    UnityEngine.Random.Range(sacabambaspisData.SpawnAddTorqueMin, sacabambaspisData.SpawnAddTorqueMax)
+                );
+                var appearanceAudioData =
+                    sacabambaspisData.AppearanceAudioDataList[
+                        UnityEngine.Random.Range(0, sacabambaspisData.AppearanceAudioDataList.Count)];
+                AudioManager.PlaySE(appearanceAudioData.AudioData);
+                this.speechBalloonController.PlayAnimation(appearanceAudioData.Serif);
+
+                this.GetAsyncTriggerEnter2DTrigger()
+                    .Subscribe(other =>
+                    {
+                        if (other.attachedRigidbody != null)
                         {
-                            // プレイヤーと接触した場合はプレイヤーの中に入ったとみなす
-                            if (other.attachedRigidbody.GetComponent<PlayerController>() != null)
+                            if (!this.isEnterPlayer)
                             {
-                                this.OnEnterPlayer(gameDesignData);
-                            }
-                            else
-                            {
-                                // 他のサカバンバスピスがプレイヤーの中に入っている場合は自分もプレイヤーの中に入る
-                                var otherSacabambaspis = other.attachedRigidbody.GetComponent<SacabambaspisController>();
-                                if(otherSacabambaspis != null && otherSacabambaspis.isEnterPlayer)
+                                // プレイヤーと接触した場合はプレイヤーの中に入ったとみなす
+                                if (other.attachedRigidbody.GetComponent<PlayerController>() != null)
                                 {
                                     this.OnEnterPlayer(gameDesignData);
                                 }
+                                else
+                                {
+                                    // 他のサカバンバスピスがプレイヤーの中に入っている場合は自分もプレイヤーの中に入る
+                                    var otherSacabambaspis =
+                                        other.attachedRigidbody.GetComponent<SacabambaspisController>();
+                                    if (otherSacabambaspis != null && otherSacabambaspis.isEnterPlayer)
+                                    {
+                                        this.OnEnterPlayer(gameDesignData);
+                                    }
+                                }
                             }
                         }
-                    }
-                    else
-                    {
-                        if (other.CompareTag("Deadline"))
+                        else
                         {
-                            Destroy(this.gameObject);
-                            var audioData = sacabambaspisData.OnExitAudioDataList[UnityEngine.Random.Range(0, sacabambaspisData.OnExitAudioDataList.Count)];
-                            AudioManager.PlaySE(audioData.AudioData);
-                            MessageBroker.GetPublisher<GameEvents.OnExitSacabambaspis>()
-                                .Publish(GameEvents.OnExitSacabambaspis.Get(
-                                        this.sacabambaspisType,
-                                        this.isEnterPlayer,
-                                        this.transform.localPosition,
-                                        audioData.Serif
+                            if (other.CompareTag("Deadline"))
+                            {
+                                Destroy(this.gameObject);
+                                var audioData = sacabambaspisData.OnExitAudioDataList[
+                                    UnityEngine.Random.Range(0, sacabambaspisData.OnExitAudioDataList.Count)];
+                                AudioManager.PlaySE(audioData.AudioData);
+                                MessageBroker.GetPublisher<GameEvents.OnExitSacabambaspis>()
+                                    .Publish(GameEvents.OnExitSacabambaspis.Get(
+                                            this.sacabambaspisType,
+                                            this.isEnterPlayer,
+                                            this.transform.localPosition,
+                                            audioData.Serif
                                         )
                                     );
+                            }
                         }
-                    }
-                })
-                .AddTo(this.GetCancellationTokenOnDestroy());
+                    })
+                    .AddTo(this.GetCancellationTokenOnDestroy());
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
         }
 
         /// <summary>
